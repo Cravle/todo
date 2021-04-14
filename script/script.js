5e83e1ea9c73660dc35308be3f021ee5e7839a18
vlad <vladoxer280502@gmail.com>
2021-04-14 17:35:13 +0300

    feat: add remove task

--- script/script.js
+++ script/script.js
@@ -3,21 +3,32 @@ const input = document.body.querySelector(".input");
 const taskItem = document.createElement("div")
 const list = document.body.querySelector(".tasks-list")
 
+
+
 input.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
     const taskItem = document.createElement("div")
     taskItem.innerHTML = `
         <li class="task wrapper">
-              <button class="check">
-                <div class="check-btn ">✔</div>
-              </button>
-              <div class="task__wrapper">
-                <div class="task__text light-black">
-                  ${input.value}
-                </div>
-              </div>
-            </li>
+          <button class="check">
+            <div class="check-btn ">✔</div>
+          </button>
+          <div class="task__wrapper">
+            <div class="task__text light-black">
+              ${input.value}
+          </div>
+        </div>
+        <button class="task__btn-remove">X</button>
+      </li>
         `
+
+
+
+
+    const deleteBtn = taskItem.querySelector(".task__btn-remove")
+    deleteBtn.addEventListener("click", () => {
+      taskItem.remove();
+    })
 
     list.appendChild(taskItem)
     input.value = ""
