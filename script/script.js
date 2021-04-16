698bf887c2125280404b9eb036b7370feb4e02a3
vlad <vladoxer280502@gmail.com>
2021-04-16 12:26:52 +0300

    ref: all functionality

--- script/script.js
+++ script/script.js
@@ -1,180 +1,141 @@
+// CONSTANTS
 const ACTIVE = 'active'
 const COMPLITED = 'complited'
 const EDIT = 'edit'
 
-const getElement = (selector) => document.body.querySelector(selector)
+// UTILS
+const getElement = selector => document.querySelector(selector)
+const isActive = task => task.status === ACTIVE
+const isCompleted = task => task.status === COMPLITED
+const generateId = () => Date.now()
 
+// VARIABLES
 const input = getElement('.input')
-const taskItem = document.createElement("div")
-const list = document.body.querySelector(".tasks-list")// ul
-const counterItem = document.body.querySelector(".status").querySelector("strong")// counter left from footer
-const footer = document.body.querySelector("footer")
-const completeAllBtn = document.querySelector("[data-all]")
-//const list = [{name: 'test', status:ACTIVE }]
+const taskItem = getElement('div')
+const list = getElement('.tasks-list') // ul
+const counterItem = getElement('.status').querySelector('strong')
+const footer = getElement('footer')
+const completeAllBtn = getElement('[data-all]')
+const ulMenu = document.querySelector('.menu')
+const menuItems = document.querySelectorAll('.menu__item')
 
-let listArr = [];
-
-let counter = 0;
-
+let taskList = []
 
 const toggleStatusTasks = () => {
-  let flag = false;
-  for (let i = 0; i < listArr.length; i++) {
-    if (listArr[i].status === ACTIVE) {
-      flag = true
-      break
-    }
-  }
+	const isSomeCompleted = taskList.some(isActive)
+	taskList = taskList.map(task => ({ ...task, status: isSomeCompleted ? COMPLITED : ACTIVE }))
 
-
-  if (flag) {
-    listArr = listArr.map(el => {
-      el.status = COMPLITED
-      return el
-    })
-
-  } else {
-    listArr = listArr.map(el => {
-      el.status = ACTIVE
-      return el
-    })
-  }
-  generateList()
-
-
-
+	checkTasksStatus()
+	renderList()
 }
 
+const checkTasksStatus = () => {
+	console.log(taskList.every(isCompleted), 'taskList.every(isCompleted)')
+	console.log(taskList, 'taskList')
+	taskList.every(isCompleted) ? completeAllBtn.classList.add('active') : completeAllBtn.classList.remove('active')
+}
 
-const generateList = () => {
-  list.innerHTML = ""
-  const newList = listArr.map(el => {
-    const taskItem = document.createElement("div")
-    if (el.status === COMPLITED) {
-      taskItem.innerHTML = `
-        <li class="task wrapper">
+const renderList = () => {
+	list.innerHTML = ''
+	const names = []
+	const newList = taskList.map(task => {
+		const counter = names.reduce((acc, name) => (name === task.name ? acc + 1 : acc), 0) // rename
+		names.push(task.name)
+
+		task.text = counter ? `${task.name}[${counter}]` : task.name
+		const taskItem = document.createElement('div')
+		if (task.status === COMPLITED) {
+			taskItem.innerHTML = `
+        <li class="task wrapper" data-id="${task.id}">
           <button class="check checked">✔</button>
           <div class="task__wrapper">
             <div class="task__text complited light-black ">
-              ${el.text}
+              ${task.text}
           </div>
         </div>
         <button class="task__btn-remove">X</button>
       </li>
         `
-      return taskItem
-    }
-    taskItem.innerHTML = `
-        <li class="task wrapper">
+			return taskItem
+		}
+		taskItem.innerHTML = `
+        <li class="task wrapper" data-id="${task.id}">
           <button class="check">✔</button>
           <div class="task__wrapper">
-            <div class="task__text light-black ">
-              ${el.text}
+            <div class="task__text light-black " >
+              ${task.text}
           </div>
         </div>
         <button class="task__btn-remove">X</button>
       </li>
         `
-    return taskItem;
-  })
+		return taskItem
+	})
 
-  let localCounter = 0;
-  listArr.forEach((el) =>
-    el.status === ACTIVE && localCounter++
-  )
+	refreshCounter()
 
-  counter = localCounter
-  refreshCounter()
-
-  newList.forEach(el => list.appendChild(el))
+	newList.forEach(el => list.appendChild(el))
 }
-
-
-list.addEventListener("click", (e) => {
-  if (e.target.classList.contains("check")) {
-    const text = e.target.parentElement.querySelector('.task__text').innerText
-    listArr = listArr.map(el => {
-      if (el.text === text) {
-        el.status === ACTIVE ? el.status = COMPLITED : el.status = ACTIVE
-      }
-      return el
-    })
-    generateList()
-  }
-
-  if (e.target.className === "task__btn-remove") {
-    const text = e.target.parentElement.querySelector('.task__text').innerText
-
-    listArr = listArr.filter(el => el.text !== text && el)
-    generateList()
-
-  }
-
-})
-
 
 const refreshCounter = () => {
-  counterItem.innerText = counter;
-  if (counter === 0 && listArr.length === 0) {
-    footer.style.display = "none"
-  } else {
-    footer.style.display = "grid"
-  }
+	const counter = taskList.reduce((acc, { status }) => (status === ACTIVE ? acc + 1 : acc), 0)
+	counterItem.innerText = counter
+	taskList.length ? footer.classList.remove('display-none') : footer.classList.add('display-none')
 }
-refreshCounter();
 
-//submit input
-input.addEventListener("keyup", (e) => {
-  if (e.key === "Enter") {
-    if (input.value.trim().length) {
-      let text = input.value;
+const onListClick = ({ target }) => {
+	const id = Number(target.parentElement.dataset.id)
+	const isDelited = target.classList.contains('task__btn-remove')
+	const isChecked = target.classList.contains('check')
+	if (isChecked) {
+		const task = taskList.find(task => task.id === id)
+		task.status = task.status === COMPLITED ? ACTIVE : COMPLITED
+	}
 
+	if (isDelited) {
+		taskList = taskList.filter(task => task.id !== id)
+	}
+	renderList()
+	checkTasksStatus()
+}
 
+const onSubmut = ({ key }) => {
+	const text = input.value
 
+	if (key !== 'Enter' || !text.trim().length) {
+		return
+	}
 
-      listArr.forEach((el, id) => {
-        if (el.text === text)
-          text = input.value + `[${id + 1}]`
-      })
+	const id = generateId()
+	taskList.push({ text, name: text, status: ACTIVE, id })
+	input.value = ''
 
-      listArr.push({ text, status: ACTIVE })
-      generateList()
-      input.value = ""
+	checkTasksStatus()
+	renderList()
+	refreshCounter()
+}
 
-      refreshCounter()
-    }
-  }
-})
+const onSortMenuClick = ({ target }) => {
+	if (target.className !== 'menu__item') {
+		return
+	}
+	menuItems.forEach(item => {
+		item.classList.remove('active')
+	})
+	target.classList.add('active')
 
+	if (target.getAttribute('data-sort') === 'all') {
+	}
+	if (target.getAttribute('data-sort') === 'active') {
+	}
+	if (target.getAttribute('data-sort') === 'complited') {
+	}
+}
 
 completeAllBtn.addEventListener('click', toggleStatusTasks)
 
+list.addEventListener('click', onListClick)
 
+input.addEventListener('keyup', onSubmut)
 
-
-const ulMenu = document.querySelector(".menu")
-const menuItems = document.querySelectorAll(".menu__item")
-
-ulMenu.addEventListener("click", (e) => {
-  if (e.target.className === "menu__item") {
-    menuItems.forEach((item) => {
-      item.classList.remove("active")
-    })
-    e.target.classList.add("active")
-    if (e.target.getAttribute("data-sort") === "all") {
-
-    }
-    if (e.target.getAttribute("data-sort") === "active") {
-
-
-    }
-    if (e.target.getAttribute("data-sort") === "complited") {
-    }
-  }
-})
-
-
-
-
-
-
+ulMenu.addEventListener('click', onSortMenuClick)
